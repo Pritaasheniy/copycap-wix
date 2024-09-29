b26aa1cd71c218447f32cd13a06a49f0bf61e000
Pritaasheniy <167381501+Pritaasheniy@users.noreply.github.com>
2024-09-29 15:40:55 +0800

    Shipping fees

--- src/pages/Home.tuckg.js
+++ src/pages/Home.tuckg.js
@@ -5,6 +5,7 @@ $w.onReady(function () {
     // Write your JavaScript here
     // edit nav menu
     // edited the logo,nave bar and and photos in home page.
+    // edited the shipping fees and regions.
     // To select an element by ID use: $w('#elementID')
 
     // Click 'Preview' to run your code
