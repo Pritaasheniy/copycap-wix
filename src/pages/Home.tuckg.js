cadf76aa7bbc9277f8cc9f814dcb78ebce4ab13f
Pritaasheniy <167381501+Pritaasheniy@users.noreply.github.com>
2024-09-29 14:54:47 +0800

    home page editings

--- src/pages/Home.tuckg.js
+++ src/pages/Home.tuckg.js
@@ -4,6 +4,7 @@
 $w.onReady(function () {
     // Write your JavaScript here
     // edit nav menu
+    // edited the logo,nave bar and and photos in home page.
     // To select an element by ID use: $w('#elementID')
 
     // Click 'Preview' to run your code
