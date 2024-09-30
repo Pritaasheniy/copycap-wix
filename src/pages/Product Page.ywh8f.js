c4a1f561c4c1edf7b2ac212ba7b65d55a05d8219
Pritaasheniy <167381501+Pritaasheniy@users.noreply.github.com>
2024-09-30 21:35:57 +0800

    wishlist of customers

--- src/pages/Product Page.ywh8f.js
+++ src/pages/Product Page.ywh8f.js
@@ -3,6 +3,8 @@
 
 $w.onReady(function () {
     // Write your JavaScript here
+    // edited wishlist of the customers.
+    // edited customers's order invoice.
 
     // To select an element by ID use: $w('#elementID')
 
